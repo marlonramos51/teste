bc41676e5afed1e75b37ad6d5f6279861ca03380
Marlon Ramos <marlon.ramos@bol.com.br>
2017-08-27 20:46:16 -0300

    primeiro

---
+++ novo.pp
@@ -0,0 +1,16 @@
+# == Class: class_name
+#
+
+class class_name {
+  # resources
+
+  file { 'name':
+    ensure => file,
+    mode   => '0644',
+  }
+
+  file { 'name':
+    ensure => file,
+    mode   => '0644',
+  }
+}
