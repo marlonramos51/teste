12fce13957d70a8f51b6a56271de0f1d47dd394d
Marlon Ramos <marlon.ramos@bol.com.br>
2017-08-31 23:11:07 -0300

    alteracoes novas

--- novo.pp
+++ novo.pp
@@ -2,25 +2,14 @@
 #
 
 class teste {
-  # resources
+  	# resources
 
   file { 'namealteradoaadd':
     ensure => file,
     mode   => '0644',
   }
 
-package { 'todos  de ffffffuma vez':
+package { 'todos aa de  sdfsffffffuma vez':
   ensure => installed,
 }
-
-  file { 'todos todos de uma vez':
-    ensure => file,
-    mode   => '0644',
-    tepm
-  }
-
-  file { 'dirbolatambem':
-    ensure => file,
-    mode   => '0644',
-  }
 }
